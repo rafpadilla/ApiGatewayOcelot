0296ef5487e67207a83093f42ea5ab97dec17f3a
Rafael Padilla <rafpadilla@hotmail.com>
2020-04-01 10:37:44 +0200

    Add project files.

---
+++ ApiCatalog/Controllers/CatalogController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiCatalog.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        public CatalogController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Catalog> Get()
+        {
+            var faker = new Faker<Catalog>();
+            faker.RuleFor(a => a.Id, (f) => f.IndexFaker);
+            faker.RuleFor(a => a.ProductName, (f, p) => f.Commerce.ProductName());
+
+            var products = faker.Generate(10);
+            return products;
+        }
+    }
+    public class Catalog
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+    }
+}
---
+++ ApiOrder/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiOrder.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        public OrdersController()
+        {
+        }
+
+        [HttpGet("{id:int?}")]
+        public IEnumerable<Order> Get(int? id)
+        {
+            var faker = new Faker<Order>();
+            faker.RuleFor(a => a.Id, (f) => f.IndexFaker);
+            faker.RuleFor(a => a.ProductId, (f) => f.IndexFaker);
+            faker.RuleFor(a => a.ProductName, (f, p) => f.Commerce.ProductName());
+
+            var orders = faker.Generate(10);
+            return orders;
+        }
+    }
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
